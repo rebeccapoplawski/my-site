5103fcedefff79bbbb795b10310b348ed3070ea0
Rebecca Poplawski <bpoplawski16@yahoo.com>
2024-11-17 12:20:13 -0600

    adding image
    adding image

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -9,12 +9,12 @@ const SideBar = () => (
   <section id="sidebar">
     <section id="intro">
       <Link to="/" className="logo">
-        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
+        <img src={`${PUBLIC_URL}/images/me.jpeg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
+        <h2>Rebecca Poplawski</h2>
         <p>
-          <a href="mailto:michael@mldangelo.com">michael@mldangelo.com</a>
+          <a href="mailto:r.m.poplawski@gmail.com">r.m.poplawski@gmail.com</a>
         </p>
       </header>
     </section>
@@ -22,13 +22,9 @@ const SideBar = () => (
     <section className="blurb">
       <h2>About</h2>
       <p>
-        Hi, I&apos;m Michael. I am a{' '}
-        <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC
-        alumnus, and the co-founder and CTO of{' '}
-        <a href="https://promptfoo.dev">Promptfoo</a>. Previously, I
-        was VP of Engineering at <a href="https://usesmileid.com">SmileID</a>,
-        co-founder and CTO of <a href="https://arthena.com">Arthena</a>, and
-        co-founded <a href="https://matroid.com">Matroid</a>.
+        Hi, I&apos;m Rebecca. I am a{' '}
+        <a href="https://icme.stanford.edu/">Senior Software Engineer </a>
+        and Indiana University Alumnus.
       </p>
       <ul className="actions">
         <li>
@@ -48,7 +44,7 @@ const SideBar = () => (
     <section id="footer">
       <ContactIcons />
       <p className="copyright">
-        &copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.
+        &copy; Rebecca Poplawski <Link to="/">rebeccapoplawski.githubio.com</Link>.
       </p>
     </section>
   </section>
